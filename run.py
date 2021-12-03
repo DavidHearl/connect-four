ebd2a943cc136bb7bb50fed9ca548144010f11d7
DavidHearl <david.hearl@outlook.com>
2021-12-03 17:12:18 +0000

    Created basic board and tested connections

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class Board:
+    """
+    Main board class. Creates fresh board for the game to be played on.
+    """
+    def __init__(self, columns, rows):
+        self.columns = columns
+        self.rows = rows
+
+    def create_board(self):
+        print("+---+---+---+---+---+---+---+")
+        print("|   |   |   |   |   |   |   |")
+        print("|   |   |   |   |   |   |   |")
+        print("|   |   |   |   |   |   |   |")
+        print("|   |   |   |   |   |   |   |")
+        print("|   |   |   |   |   |   |   |")
+        print("|   |   |   |   |   |   |   |")
+        print("+---+---+---+---+---+---+---+")
+        
+
+size = Board(7, 6)
+print(size.columns)
+print(size.create_board())
