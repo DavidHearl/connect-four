4ca457ba17b3fcb612c631221adb89f6f1a5dd3c
DavidHearl <david.hearl@outlook.com>
2021-12-04 21:00:11 +0000

    Created move function to check for filled space and change turn

--- run.py
+++ run.py
@@ -19,9 +19,32 @@ class ConnectFour:
         print("+---+---+---+---+---+---+---+")
         print("  0   1   2   3   4   5   6  ")
 
+    def move(self, column):
+        # Write Checks for winner, check if they have pick 0-6, check is column is full.
+        selectedColumn = self.board[:,column]
+        filled = True
+        height = 5
+        while filled:
+            if self.board[height, column] == 0:
+                filled = False
+                break
+            height -= 1
+
+        self.board[height, column] = self.to_play
+        
+        if self.to_play == 1:
+            self.to_play = 2
+        else:
+            self.to_play = 1
+
+        #print(height)        
+        #print(selectedColumn)
+
 
 game = ConnectFour()
-game.board[0,1] = 1
-game.board[1,0] = 1
-print(game.board)
+game.board[5,1] = 2
+game.printBoard()
+game.to_play = 1
+game.move(1)
+print(game.to_play)
 game.printBoard()
