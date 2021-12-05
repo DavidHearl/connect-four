8efe315ae1d436adfc8a0dbd37fc2a9e2dc45eb8
DavidHearl <david.hearl@outlook.com>
2021-12-05 21:56:41 +0000

    Adapted play game function till someone wins

--- run.py
+++ run.py
@@ -8,16 +8,16 @@ class ConnectFour:
         self.board = np.zeros(shape= (6,7), dtype=int)
         self.winner = None
         self.to_play = None
+        self.markers = [" ", "X", "O"]
     
     def printBoard(self):
         """
         Assigns values to markers and creates board
         """
-        markers = [" ", "X", "O"]
         print("+---+---+---+---+---+---+---+")
         for row in range(6):
             for column in range(7):
-                print("| " + markers[self.board[row,column]] + " ", end="")
+                print("| " + self.markers[self.board[row,column]] + " ", end="")
             print("|")
         print("+---+---+---+---+---+---+---+")
         print("  0   1   2   3   4   5   6  ")
@@ -48,19 +48,16 @@ class ConnectFour:
         Sets player as 1 then gets PC to select a random point between 0 and 6
         """
         self.to_play = 1
-        for _ in range(16):
+        win = None
+        while not win:
             if self.to_play == 1:
                 self.move(int(input("Enter Column: ")))
             else:
-                self.move(0)
-                self.move(1)
-                self.move(2)
-                #self.move(np.random.randint(7))
-                
-            print(self.board[:,0])
-            print(self.checkWinner())
-            print(self.board.diagonal(), print(self.board.diagonal(1))),
+                self.move(np.random.randint(7))
+
             self.printBoard()
+            win = self.checkWinner()
+        print(f"Player {self.markers[win]} has won the game")
 
     def checkFour(self, values):
         """
@@ -117,14 +114,15 @@ class ConnectFour:
             if result:
                 return result        
 
-#def main():
-    #game = ConnectFour()
-    #game.printBoard()
-    #game.playGame()
+def main():
+    game = ConnectFour()
+    game.printBoard()
+    game.playGame()
 
 print("Welcome to connect four, to play please enter the column you would like to play in")
-#main()
+main()
 
+"""
 game = ConnectFour()
 np.fill_diagonal(game.board,val=2)
 game.printBoard()
@@ -132,6 +130,8 @@ print(game.checkWinner())
 game.board = np.fliplr(game.board)
 print(game.board)
 print(game.checkWinner())
+"""
+
 
 """
 Currently have function to check for 4 in a row in an array
